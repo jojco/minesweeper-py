47013f4cc3ab4ca65cd51188bf5af11f94fbe0cc
jozef.janecek <jozef.janecek@asseco-ceit.com>
2023-01-18 18:31:47 +0000

    config ch.

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 This is simple minesweeper console game.
 Enyoj and have a fun.
 """
+from typing import Dict
 import yaml
 from src.game import Minesweeper
 
 
-if __name__ == "__main__":
-    with open("config.yaml", "r", encoding="utf-8") as file:
+def parse_yaml(filepath: str) -> Dict:
+
+    with open(filepath, 'r', encoding="utf-8") as file:
         data = yaml.safe_load(file)
-        print(data)
+    return data
+
+
+if __name__ == "__main__":
+    config = parse_yaml("config.yaml")
+    print(config)
 
     game = Minesweeper(10, 10, 10)
     game.print_board()
